95c6eddbdc018ffa5a3cdab090ea5602e5089266
Luiz Calazans <Luiz.Calazans@processor.com.br>
2019-10-31 08:53:42 -0300

    First Commit

---
+++ ProjEventWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjEventWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
