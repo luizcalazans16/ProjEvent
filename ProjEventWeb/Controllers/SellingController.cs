4007496f2c857d0ae40fef8e61f07245e32c8104
Luiz Calazans <luizfernando.calazans8@gmail.com>
2019-11-27 23:00:00 -0300

    Button

--- ProjEventWeb/Controllers/SellingController.cs
+++ ProjEventWeb/Controllers/SellingController.cs
@@ -53,6 +53,16 @@ namespace ProjEventWeb.Controllers
             return View();
         }
 
+        //POST
+        public ActionResult BuyTicket() {
+            var sell = new UserEvent();
+            var x = EventId;
+
+        }
+
+
+
+
         // POST: Selling/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
